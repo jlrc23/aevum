a317f3d9fdb90aa7b3dc7ecee1b8aab6f34fb005
jjos_luis_rosado_c_rdenas <jlrosado@toc-goldratt.com>
2013-10-08 13:52:01 -0500

    definiendo datos de la base de datos

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'aevum');
+define('DB_NAME', 'aevum'); //
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'root'); // usrAevum
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', ''); // ISI2013aev
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
